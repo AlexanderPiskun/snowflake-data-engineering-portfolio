1886665d1c858667c4f7b6d14f19a39b475cb6c8
AlexanderPiskun <apiskun@gmail.com>
2026-01-07 15:43:31 -0700

    Create 04_NOAA_WA_Create_Staging_Setup.sql
    STAGING Layer (Transform & Normalize)

---
+++ project-1-weather-analytics/sql/04_NOAA_WA_Create_Staging_Setup.sql
@@ -0,0 +1,38 @@
+--STAGING Layer (Transform & Normalize)
+USE WAREHOUSE NOAA_WH;
+USE ROLE NOAA_ENGINEER;
+USE DATABASE NOAA_WEATHER_DB;
+USE SCHEMA STAGING;
+
+--Stations (Dimension-ready)
+CREATE OR REPLACE TABLE STG_WEATHER_STATIONS AS
+SELECT
+  noaa_weather_station_id   AS station_id,
+  noaa_weather_station_name AS station_name,
+  country_name              AS country,
+  state_name                AS state,
+  latitude,
+  longitude,
+  elevation,
+  CURRENT_TIMESTAMP         AS load_ts
+FROM RAW.NOAA_WEATHER_STATION_INDEX;
+
+--Weather Metrics (Standardized + type casting)
+CREATE OR REPLACE TABLE STG_WEATHER_METRICS AS
+SELECT
+  noaa_weather_station_id AS station_id,
+  variable_name           AS metric_name,
+  datetime::DATE          AS observation_date,
+  value::FLOAT            AS metric_value,
+  CURRENT_TIMESTAMP       AS load_ts
+FROM RAW.NOAA_WEATHER_METRICS_TS;
+
+--Create a canonical staging view
+CREATE OR REPLACE VIEW STAGING.V_STG_WEATHER_METRICS AS
+SELECT
+  noaa_weather_station_id AS station_id,
+  variable_name           AS metric_name,
+  datetime::DATE          AS observation_date,
+  value::FLOAT            AS metric_value,
+  CURRENT_TIMESTAMP       AS load_ts
+FROM RAW.NOAA_WEATHER_METRICS_TS;
