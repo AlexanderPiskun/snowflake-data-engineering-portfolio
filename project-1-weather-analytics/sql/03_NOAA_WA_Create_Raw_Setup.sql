3645a00706a22b9a943a2f6dd3fcd50b958bce00
AlexanderPiskun <apiskun@gmail.com>
2026-01-12 14:32:30 -0700

    Update 03_NOAA_WA_Create_Raw_Setup.sql
    Added code to automatically create RO DATABASE SNOWFLAKE_PUBLIC_DATA_FREE  from snowflake public share

--- project-1-weather-analytics/sql/03_NOAA_WA_Create_Raw_Setup.sql
+++ project-1-weather-analytics/sql/03_NOAA_WA_Create_Raw_Setup.sql
@@ -1,7 +1,26 @@
 -- RAW Layer (Marketplace Tables)
 USE WAREHOUSE NOAA_WH;
-USE ROLE NOAA_ENGINEER;
 USE DATABASE NOAA_WEATHER_DB;
+USE ROLE ACCOUNTADMIN;
+
+
+--Create a RO shouce database from share
+SHOW AVAILABLE LISTINGS LIKE '%Snowflake Public Data%';
+
+--2025-09-05 14:42:32.911 -0700	GZTSZ290BV255	Snowflake Public Data (Free)	GZTSZAS2KCS	2025-09-26 11:30:57.928 -0700	2025-09-17 14:14:28.000 -0700	ALL	false	false	false	false	true	true	false	EXTERNAL			false	
+
+-- 3. Accept the legal terms for the listing (Required for automation)
+-- Replace 'GZTSZ290BV255' with the actual Global Name found in step 2 if different
+CALL SYSTEM$ACCEPT_LEGAL_TERMS('DATA_EXCHANGE_LISTING', 'GZTSZ290BV255');
+
+-- 4. Create the database from the listing
+CREATE DATABASE SNOWFLAKE_PUBLIC_DATA_FREE 
+  FROM LISTING 'GZTSZ290BV255';
+
+-- 5. Grant access to other roles in your account
+GRANT IMPORTED PRIVILEGES ON DATABASE SNOWFLAKE_PUBLIC_DATA_FREE TO ROLE NOAA_ENGINEER;
+
+USE ROLE NOAA_ENGINEER;
 USE SCHEMA RAW;
 
 -- Load marketplace snapshot into local table. 
